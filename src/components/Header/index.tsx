85b8b7b18ca11e5e9d79a7e4b5ad77a1592500f4
mauricioholanda <mauriciofholanda@gmail.com>
2021-08-31 09:03:18 -0300

    feat: header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,16 @@
+import illustrationImg from "../../assets/images/marvel.svg";
+import "./header.scss";
+import { Button } from "../Button";
+
 export function Header() {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className="content">
+        <img src={illustrationImg} alt="marvel" />
+        <div>
+          <Button isOutlined>Logout</Button>
+        </div>
+      </div>
+    </header>
+  );
 }
