aa378c1c794751e163871b2581b080b8febd69fc
mauricioholanda <mauriciofholanda@gmail.com>
2021-09-12 13:04:21 -0300

    feat: create a private route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,22 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Login from "../view/login/index";
 import home from "../view/home/index";
+import characterDetail from "../view/characterDetail/index";
 
 import { AuthContextProvider } from "../context/AuthContext";
+import PrivateRoute from "./privateRoute";
 
 export const Routes = () => (
   <BrowserRouter>
     <AuthContextProvider>
       <Switch>
         <Route path="/" exact component={Login} />
-        <Route path="/home" exact component={home} />
+        <PrivateRoute path="/home" exact component={home} />
+        <PrivateRoute
+          path="/characterDetail/:id"
+          exact
+          component={characterDetail}
+        />
       </Switch>
     </AuthContextProvider>
   </BrowserRouter>
